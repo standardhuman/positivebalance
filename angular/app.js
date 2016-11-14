4d6648e8c6321831e5edff6d82587ce53a4623f8
Brian Cline <standardhuman@gmail.com>
2016-11-14 15:36:56 -0800

    move js to app.js

--- angular/app.js
+++ angular/app.js
@@ -1,9 +1,8 @@
-var positiveBalance = angular.module('positiveBalance', [])
+var positiveBalance = angular.module('positiveBalance', []).controller('ActivityController', function ActivityController($scope) {
+    $scope.create = {}
 
-    .controller('ActivityController', function ActivityController($scope) {
-        $scope.create = {}
-
-        $scope.activityList = [{
+    $scope.activityList = [
+        {
             name: 'run',
             moreorless: 1,
             quantity: 1,
@@ -19,33 +18,34 @@ var positiveBalance = angular.module('positiveBalance', [])
             weight: 6,
             didido: 1,
             howmanyunits: 0
-        }];
-
-        $scope.combinedNum = {};
-        $scope.dailyTotal = function() {
-            var total = 0;
-            for (var i = 0; i < $scope.activityList.length; i++) {
-                $scope.activityList[i].points = (($scope.activityList[i].quantity * $scope.activityList[i].howmanyunits) * $scope.activityList[i].weight / 10) * $scope.activityList[i].moreorless;
-                total += $scope.activityList[i].points;
-            }
-            $scope.combinedNum.total = total;
-
         }
-        $scope.addNewActivity = function() {
-            var index = $scope.activityList.findIndex(function(activity) {
-              return activity.name === $scope.create.newActivityName;
-            });
-            console.log("index:", index);
-            if (index !== -1) {
-              $scope.activityList[index].unit = $scope.create.newActivityUnit;
-              $scope.activityList[index].quantity = $scope.create.newActivityAmount;
-              $scope.activityList[index].moreorless = $scope.create.newActivityMoreorless;
-              $scope.activityList[index].weight = $scope.create.newActivityWeight;
-              $scope.activityList[index].points = 0;
-              $scope.activityList[index].howmanyunits = 0;
-              console.log("activity List after update", $scope.activityList)
-            } else {
-              $scope.activityList.push({
+    ];
+
+    $scope.combinedNum = {};
+    $scope.dailyTotal = function() {
+        var total = 0;
+        for (var i = 0; i < $scope.activityList.length; i++) {
+            $scope.activityList[i].points = (($scope.activityList[i].quantity * $scope.activityList[i].howmanyunits) * $scope.activityList[i].weight / 10) * $scope.activityList[i].moreorless;
+            total += $scope.activityList[i].points;
+        }
+        $scope.combinedNum.total = total;
+
+    }
+    $scope.addNewActivity = function() {
+        var index = $scope.activityList.findIndex(function(activity) {
+            return activity.name === $scope.create.newActivityName;
+        });
+        console.log("index:", index);
+        if (index !== -1) {
+            $scope.activityList[index].unit = $scope.create.newActivityUnit;
+            $scope.activityList[index].quantity = $scope.create.newActivityAmount;
+            $scope.activityList[index].moreorless = $scope.create.newActivityMoreorless;
+            $scope.activityList[index].weight = $scope.create.newActivityWeight;
+            $scope.activityList[index].points = 0;
+            $scope.activityList[index].howmanyunits = 0;
+            console.log("activity List after update", $scope.activityList)
+        } else {
+            $scope.activityList.push({
                 name: $scope.create.newActivityName,
                 unit: $scope.create.newActivityUnit,
                 quantity: $scope.create.newActivityAmount,
@@ -54,17 +54,17 @@ var positiveBalance = angular.module('positiveBalance', [])
                 points: 0,
                 howmanyunits: 0
             });
-          }
         }
+    }
 
-        $scope.convertMoreorLess = function() {
-            $scope.moreorlessModel = {};
-            console.log($scope.create.newActivityMoreorless);
-            if ($scope.create.newActivityMoreorless == 1) {
-                $scope.moreorlessModel.moreorless = "more";
-            } else {
-                $scope.moreorlessModel.moreorless = "less";
-            }
-            return $scope.moreorlessModel.moreorless;
+    $scope.convertMoreorLess = function() {
+        $scope.moreorlessModel = {};
+        console.log($scope.create.newActivityMoreorless);
+        if ($scope.create.newActivityMoreorless == 1) {
+            $scope.moreorlessModel.moreorless = "more";
+        } else {
+            $scope.moreorlessModel.moreorless = "less";
         }
-    });
+        return $scope.moreorlessModel.moreorless;
+    }
+});
