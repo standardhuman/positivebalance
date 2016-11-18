82a2fd8bf935d1f824420c940bf8cf26298c2f51
Brian Cline <standardhuman@gmail.com>
2016-11-18 15:16:50 -0800

    new react version init

---
+++ react/src/client/app/index.jsx
@@ -0,0 +1,48 @@
+console.log('Hello World!');
+class CreateNewActivity extends React.Component {
+    render() {
+        return
+            <div>
+                <h1>Create New Activity</h1>
+            </div>
+        /*html {this.props/this.state.varName} */
+
+    }
+}
+
+class SaveNewActivity extends React.Component {
+    render() {
+        return
+    }
+}
+
+class UpdateActivity extends React.Component {
+    render() {
+        return
+    }
+}
+
+class ActivitySummary extends React.Component {
+    render() {
+        return
+    }
+}
+
+class PointsSummary extends React.Component {
+    render() {
+        return
+    }
+}
+
+class PositiveBalance extends React.Component {
+    render() {
+        return (
+            <div>
+                <CreateNewActivity/>
+                <UpdateActivity/>
+                <ActivitySummary/>
+            </div>
+        )
+    }
+}
+ReactDom.render(< document.getElementById('container'))
