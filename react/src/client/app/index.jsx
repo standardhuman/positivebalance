267fc05e2ee1ec7aa189ce7f8b6914bc0b5c400b
Brian Cline <standardhuman@gmail.com>
2016-11-21 14:30:22 -0800

    work on setting state

--- react/src/client/app/index.jsx
+++ react/src/client/app/index.jsx
@@ -5,7 +5,42 @@ class CreateNewActivity extends React.Component {
     render() {
         return (
             <div>
-                <h1>Create New Activity</h1>
+                <p>{this.state.activities}</p>
+                <form id="newActivity">
+                    <h1>
+                        <p className="title">Create new activity</p>
+                    </h1>
+                    <div className="inputs">
+                        <p>I would like to
+                            <label htmlFor="activityName"></label>
+                            <input type="text" id="activity_name" name="activity_name" placeholder="run, drink, meditate..."/>
+                            <label htmlFor="more">more</label>
+                            <input type="radio" value="1" id="more" name="moreorless"/>
+                            <label htmlFor="less">less</label>
+                            <input type="radio" value="-1" id="less" name="moreorless"/>
+                        </p>
+                    </div>
+                    <div className="inputs">
+                        <p>
+                            <label htmlFor="activity_unit">
+                                'When I do {this.props.activities}, the smallest amount I might ACTIVITYNAME is'
+                            </label>
+                            <label htmlFor="min_qty"></label>
+                            <input type="number" id="min_qty" name="min_qty" placeholder="1, 2..."/>
+                            <input type="text" id="activity_unit" name="activity_unit" placeholder="miles, drink, minutes..."/>
+                        </p>
+                    </div>
+                    <div className="inputs">
+                        <label htmlFor="activity_weight">On a scale of 1-10, with 10 being most important, how important is it to me that I ACTIVITYNAME MOREORLESS
+                        </label>
+                        <br/>
+                        <input type="number" id="activity_weight" name="activity_weight" min="1" max="10" placeholder="1-10"/>
+                    </div>
+                    <br/>
+                    <div className="button">
+                        <button type="button">Save new activity</button>
+                    </div>
+                </form>
             </div>
         /*html {this.props/this.state.varName} */
         )
@@ -53,35 +88,41 @@ class PointsSummary extends React.Component {
 }
 
 class PositiveBalance extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {ACTIVITIES: [
+            {
+                name: 'run',
+                moreorless: 1,
+                quantity: 1,
+                unit: 'mile',
+                weight: 5,
+                didido: 1,
+                howmanyunits: 0
+            }, {
+                name: 'drink',
+                moreorless: -1,
+                quantity: 1,
+                unit: 'drinks',
+                weight: 6,
+                didido: 1,
+                howmanyunits: 0
+            }
+        ]
+    }
+    }
+
     render() {
-        return <div>
-            <CreateNewActivity/>
-            <PointsSummary/>
-            <ActivitySummary/>
-            <UpdateActivity/>
-        </div>
+        return (
+            <div>
+                <CreateNewActivity activities={this.state.ACTIVITIES}/>
+                <PointsSummary/>
+                <ActivitySummary/>
+                <UpdateActivity/>
+            </div>
+        )
     }
 }
 
-var ACTIVITIES = [
-    {
-        name: 'run',
-        moreorless: 1,
-        quantity: 1,
-        unit: 'mile',
-        weight: 5,
-        didido: 1,
-        howmanyunits: 0
-    }, {
-        name: 'drink',
-        moreorless: -1,
-        quantity: 1,
-        unit: 'drinks',
-        weight: 6,
-        didido: 1,
-        howmanyunits: 0
-    }
-];
-
 ReactDOM.render(
-    <PositiveBalance activities={ACTIVITIES}/>, document.getElementById('container'))
+    <PositiveBalance/>, document.getElementById('container'))
